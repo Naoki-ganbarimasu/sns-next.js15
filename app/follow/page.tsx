a7949198eca6d1f9f9ee0e7672aa38dd2696686e
naokicode <kitajimanaoki.0917@icloud.com>
2024-09-15 03:26:39 +0900

    解決

--- app/follow/page.tsx
+++ app/follow/page.tsx
@@ -4,7 +4,6 @@ import RightSidebar from "@/components/component/RightSidebar";
 import { auth } from "@clerk/nextjs/server";
 
 export default function Home() {
-    const { userId } = auth();
   return (
     <div className="h-full grid grid-cols-1 md:grid-cols-[240px_1fr_240px] gap-6 p-6 overflow-hidden">
       <LeftSidebar />
